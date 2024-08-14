b61908d3ad5ac19459ae388c51a27ebdbebbb3d6
hattrick <dasha.khitrik@gmail.com>
2024-08-14 23:59:15 +0300

    refactor: rename "prev" in meaningful values, refactor useSetTasksToLocalstorage

--- src/utils/hooks/useSetTasksToLocalStorage.ts
+++ src/utils/hooks/useSetTasksToLocalStorage.ts
@@ -1,31 +1,26 @@
 import { useEffect } from 'react'
 
 import { TASKS_KEY } from '@constants/magicValues'
-import { Task } from '@customTypes/task'
+import { Task, Tasks } from '@customTypes/task'
 import { DatePickerService } from '@utils/DatePickerService'
 import { getTasksFromLocalStorage } from '@utils/getTasksFromLocalStorage'
 
 export const useSetTasksToLocalstorage = (tasks: Task[], date: string, tasksPickerService: DatePickerService) => {
+    const updateTasks = (items: Tasks) => {
+        tasksPickerService.setTasks(items)
+        localStorage.setItem(TASKS_KEY, JSON.stringify(items))
+    }
+
     useEffect(() => {
+        const currentTasks = getTasksFromLocalStorage()
         if (tasks.length) {
-            const items = {
-                ...getTasksFromLocalStorage(),
-                [date]: {
-                    tasks,
-                },
-            }
-            tasksPickerService.setTasks(items)
-            localStorage.setItem(TASKS_KEY, JSON.stringify(items))
-        } else {
-            const datesWithTasks = Object.keys(tasksPickerService.getTasks())
-            if (datesWithTasks.includes(date)) {
-                const items = {
-                    ...getTasksFromLocalStorage(),
-                }
-                delete items[date]
-                tasksPickerService.setTasks(items)
-                localStorage.setItem(TASKS_KEY, JSON.stringify(items))
-            }
+            updateTasks({
+                ...currentTasks,
+                [date]: { tasks },
+            })
+        } else if (currentTasks[date]) {
+            const { [date]: _, ...remainingItems } = currentTasks
+            updateTasks(remainingItems)
         }
     }, [tasks])
 }
